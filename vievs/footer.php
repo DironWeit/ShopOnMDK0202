8054a94ba4f6c3c604c29c8dd0ca59476769d3ee
my_pc <arseneiantonov27@gmail.com>
2026-09-24 12:34:05 +0300

    first commit

---
+++ vievs/footer.php
@@ -0,0 +1,123 @@
+<!-- ==================== Footer Two Start Here ==================== -->
+<footer class="footer-two section-bg position-relative z-index-1 overflow-hidden">
+
+  <img src="assets/images/gradients/footer-gradient-bg.png" alt="" class="bg--gradient">
+
+  <img src="assets/images/shapes/footer-pattern1.png" alt="" class="position-absolute end-0 top-0 z-index--1">
+  <img src="assets/images/shapes/footer-pattern2.png" alt="" class="position-absolute start-0 top-0 z-index--1">
+
+  <div class="footer-inner padding-y-120">
+    <div class="container container-two">
+      <div class="row gy-5">
+        <div class="col-xl-3 col-sm-6">
+          <div class="footer-item">
+            <div class="footer-item__logo">
+              <a href="index.html"> <img src="assets/images/logo/logo.png" alt=""></a>
+            </div>
+            <p class="footer-item__desc">Lorem consultancy elitsed do eiusmod tempor inci didunt ut labore dolore magna
+              aliqua sed do eiusmod.</p>
+            <div class="footer-item__social">
+              <ul class="social-list">
+                <li class="social-list__item">
+                  <a href="https://www.facebook.com" class="social-list__link flx-center"><i
+                      class="fab fa-facebook-f"></i></a>
+                </li>
+                <li class="social-list__item">
+                  <a href="https://www.twitter.com" class="social-list__link flx-center"> <i
+                      class="fab fa-twitter"></i></a>
+                </li>
+                <li class="social-list__item">
+                  <a href="https://www.linkedin.com" class="social-list__link flx-center"> <i
+                      class="fab fa-linkedin-in"></i></a>
+                </li>
+                <li class="social-list__item">
+                  <a href="https://www.pinterest.com" class="social-list__link flx-center"> <i
+                      class="fab fa-pinterest-p"></i></a>
+                </li>
+                <li class="social-list__item">
+                  <a href="https://www.pinterest.com" class="social-list__link flx-center"> <i
+                      class="fab fa-youtube"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-2 col-sm-6 col-xs-6">
+          <div class="footer-item">
+            <h5 class="footer-item__title">Useful Link</h5>
+            <ul class="footer-menu">
+              <li class="footer-menu__item"><a href="all-product.html" class="footer-menu__link">Product </a></li>
+              <li class="footer-menu__item"><a href="product-details.html" class="footer-menu__link">Product Details</a>
+              </li>
+              <li class="footer-menu__item"><a href="profile.html" class="footer-menu__link">Profile </a></li>
+              <li class="footer-menu__item"><a href="cart.html" class="footer-menu__link">Shopping Cart</a></li>
+              <li class="footer-menu__item"><a href="dashboard.html" class="footer-menu__link">Dashboard</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-xl-1 d-xl-block d-none"></div>
+        <div class="col-xl-3 col-sm-6 col-xs-6">
+          <div class="footer-item">
+            <h5 class="footer-item__title">Quick Links </h5>
+            <ul class="footer-menu">
+              <li class="footer-menu__item"><a href="dashboard.html" class="footer-menu__link">Dashboard </a></li>
+              <li class="footer-menu__item"><a href="login.html" class="footer-menu__link">Login </a></li>
+              <li class="footer-menu__item"><a href="register.html" class="footer-menu__link">Register</a></li>
+              <li class="footer-menu__item"><a href="blog.html" class="footer-menu__link">Blog </a></li>
+              <li class="footer-menu__item"><a href="blog-details.html" class="footer-menu__link">Blog Details</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 col-xs-6">
+          <div class="footer-item">
+            <h5 class="footer-item__title">Categoreis</h5>
+            <ul class="footer-menu">
+              <li class="footer-menu__item"><a href="all-product.html" class="footer-menu__link">WordPress</a></li>
+              <li class="footer-menu__item"><a href="all-product.html" class="footer-menu__link">React</a></li>
+              <li class="footer-menu__item"><a href="all-product.html" class="footer-menu__link">HTML</a></li>
+              <li class="footer-menu__item"><a href="all-product.html" class="footer-menu__link">Laravel</a></li>
+              <li class="footer-menu__item"><a href="all-product.html" class="footer-menu__link">Figma</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- bottom Footer Two -->
+  <div class="bottom-footer-two">
+    <div class="container container-two">
+      <div class="bottom-footer__inner flx-between gap-3">
+        <p class="bottom-footer__text font-14"> Copyright &copy; 2024 DPmarket, All rights reserved.</p>
+        <div class="footer-links">
+          <a href="#" class="footer-link font-14">Terms of service</a>
+          <a href="#" class="footer-link font-14">Privacy Policy</a>
+          <a href="contact.html" class="footer-link font-14">cookies</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- ==================== Footer Two End Here ==================== -->
+
+
+<!-- Jquery js -->
+<script src="assets/js/jquery-3.7.1.min.js"></script>
+<!-- Bootstrap Bundle Js -->
+<script src="assets/js/boostrap.bundle.min.js"></script>
+<!-- CountDown -->
+<script src="assets/js/countdown.js"></script>
+<!-- counter up -->
+<script src="assets/js/counterup.min.js"></script>
+<!-- Slick js -->
+<script src="assets/js/slick.min.js"></script>
+<!-- magnific popup -->
+<script src="assets/js/jquery.magnific-popup.js"></script>
+<!-- apex chart -->
+<script src="assets/js/apexchart.js"></script>
+
+<!-- main js -->
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
