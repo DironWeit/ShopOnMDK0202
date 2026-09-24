4f03203f9fd8e8ba56bd3e2f9df53b4a42d0cf28
my_pc <arseneiantonov27@gmail.com>
2026-09-24 12:40:44 +0300

    Изменяем html на php

---
+++ vievs/contact.php
@@ -0,0 +1,154 @@
+<!-- ======================== Breadcrumb Two Section Start ===================== -->
+<section class="breadcrumb border-bottom p-0 d-block section-bg position-relative z-index-1">
+
+    <div class="breadcrumb-two">
+        <img src="assets/images/gradients/breadcrumb-gradient-bg.png" alt="" class="bg--gradient">
+        <div class="container container-two">
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <div class="breadcrumb-two-content text-center">
+    
+                        <ul class="breadcrumb-list flx-align gap-2 mb-2 justify-content-center">
+                            <li class="breadcrumb-list__item font-14 text-body">
+                                <a href="index.html" class="breadcrumb-list__link text-body hover-text-main">Home</a>
+                            </li>
+                            <li class="breadcrumb-list__item font-14 text-body">
+                                <span class="breadcrumb-list__icon font-10"><i class="fas fa-chevron-right"></i></span>
+                            </li>
+                            <li class="breadcrumb-list__item font-14 text-body">
+                                <span class="breadcrumb-list__text">Contact</span>
+                            </li>
+                        </ul>
+                        
+                        <h3 class="breadcrumb-two-content__title mb-0 text-capitalize">Contact Us</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ======================== Breadcrumb Two Section End ===================== -->
+
+<!-- =========================== Contact Section Start ========================== -->
+<section class="contact padding-t-120 padding-b-60 section-bg position-relative z-index-1 overflow-hidden">
+    <img src="assets/images/gradients/banner-two-gradient.png" alt="" class="bg--gradient">
+    <img src="assets/images/shapes/pattern-five.png" class="position-absolute end-0 top-0 z-index--1" alt="">
+
+    <div class="container container-two">
+        <div class="row gy-4">
+            <div class="col-lg-5">
+                <div class="contact-info">
+                    <h3 class="contact-info__title">Get in touch with us today</h3>
+                    <p class="contact-info__desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum rem facere labore cupiditate sint? Animi quis illo suscipit autem cum.</p>
+
+                    <div class="contact-info__item-wrapper flx-between gap-4">
+                        <div class="contact-info__item">
+                            <span class="contact-info__text text-capitalize d-block mb-1">Give Us A Call</span>
+                            <a href="tel:01812345678" class="contact-info__link font-24 fw-500 text-heading hover-text-main">01812345678</a>
+                        </div>
+                        <div class="contact-info__item">
+                            <span class="contact-info__text text-capitalize d-block mb-1">Give Us An Email</span>
+                            <a href="tel:dpmarket@gmail.com" class="contact-info__link font-24 fw-500 text-heading hover-text-main">dpmarket@gmail.com</a>
+                        </div>
+                    </div>
+
+                    <div class="mt-24">
+                        <ul class="social-list">
+                            <li class="social-list__item">
+                                <a href="https://www.facebook.com" class="social-list__link text-heading flx-center"><i class="fab fa-facebook-f"></i></a>
+                            </li>
+                            <li class="social-list__item">
+                                <a href="https://www.twitter.com" class="social-list__link text-heading flx-center"> <i class="fab fa-twitter"></i></a>
+                            </li>
+                            <li class="social-list__item">
+                                <a href="https://www.linkedin.com" class="social-list__link text-heading flx-center"> <i class="fab fa-linkedin-in"></i></a>
+                            </li>
+                            <li class="social-list__item">
+                                <a href="https://www.pinterest.com" class="social-list__link text-heading flx-center"> <i class="fab fa-pinterest-p"></i></a>
+                            </li>
+                            <li class="social-list__item">
+                                <a href="https://www.pinterest.com" class="social-list__link text-heading flx-center"> <i class="fab fa-youtube"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="col-lg-7 ps-lg-5">
+                <div class="card common-card p-sm-4">
+                    <div class="card-body">
+                        <form action="#" autocomplete="off">
+                            <div class="row gy-4">
+                                <div class="col-sm-6 col-xs-6">
+                                    <label for="name" class="form-label mb-2 font-18 font-heading fw-600">Full Name</label>
+                                    <input type="text" class="common-input common-input--grayBg border" id="name" placeholder="Your name here">
+                                </div>
+                                <div class="col-sm-6 col-xs-6">
+                                    <label for="email" class="form-label mb-2 font-18 font-heading fw-600">Your Mail</label>
+                                    <input type="email" class="common-input common-input--grayBg border" id="email" placeholder="Your email here ">
+                                </div>  
+                                <div class="col-sm-12">
+                                    <label for="message" class="form-label mb-2 font-18 font-heading fw-600">Your Message</label>
+                                    <textarea class="common-input common-input--grayBg border" id="message" placeholder="Write Your Message Here"></textarea>
+                                </div>
+                                <div class="col-sm-12">
+                                    <button class="btn btn-main btn-lg pill w-100"> Submit Now </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- =========================== Contact Section End ========================== -->
+
+<!-- ===================== Newsletter Two Start ============================ -->
+
+<section class="newsletter-two padding-t-60 padding-b-120 section-bg position-relative z-index-1 overflow-hidden">
+  
+    <img src="assets/images/gradients/breadcrumb-gradient-bg.png" alt="" class="bg--gradient">
+    <img src="assets/images/shapes/element-moon3.png" alt="" class="element one">
+    <img src="assets/images/shapes/element-moon1.png" alt="" class="element three">
+    
+    <div class="container container-two">
+        <div class="flx-between gap-3">
+            <div class="newsletter-two-content">
+                <h3 class="newsletter-two-content__title mb-3">Get all items for just $59!</h3>
+                <p class="newsletter-two-content__desc text-heading font-18">With our broad assortment of components, making and tweaking designs becomes natural. Disregard coding and partake in our topics.</p>
+            </div>
+            <a href="all-product.html" class="btn btn-main btn-lg pill">Grabe All Product</a>
+        </div>
+    </div>
+</section>
+<!-- ===================== Newsletter Two End ============================ -->
+
+
+<!-- ======================== Brand Section Start ========================= -->
+<div class="brand ">
+    <div class="container container">
+        <div class="brand-slider">
+            <div class="brand-item d-flex align-items-center justify-content-center">
+                <img src="assets/images/thumbs/brand-img1.png" alt="">
+            </div>
+            <div class="brand-item d-flex align-items-center justify-content-center">
+                <img src="assets/images/thumbs/brand-img2.png" alt="">
+            </div>
+            <div class="brand-item d-flex align-items-center justify-content-center">
+                <img src="assets/images/thumbs/brand-img3.png" alt="">
+            </div>
+            <div class="brand-item d-flex align-items-center justify-content-center">
+                <img src="assets/images/thumbs/brand-img4.png" alt="">
+            </div>
+            <div class="brand-item d-flex align-items-center justify-content-center">
+                <img src="assets/images/thumbs/brand-img5.png" alt="">
+            </div>
+            <div class="brand-item d-flex align-items-center justify-content-center">
+                <img src="assets/images/thumbs/brand-img3.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ======================== Brand Section End ========================= -->
+
